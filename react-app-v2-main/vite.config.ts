cded6614ac991bbbc5886b4c8e7ede81b811e3aa
Roman Ponomarchuk <r.ponomarchuk@inside360.tv>
2025-03-04 22:33:12 +0100

    init front app

---
+++ react-app-v2-main/vite.config.ts
@@ -0,0 +1,29 @@
+import { defineConfig } from 'vitest/config';
+import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
+import svgr from 'vite-plugin-svgr';
+import tailwindcss from '@tailwindcss/vite';
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    tailwindcss(),
+    svgr({
+      svgrOptions: {
+        plugins: ['@svgr/plugin-svgo', '@svgr/plugin-jsx'],
+        svgoConfig: {
+          floatPrecision: 2,
+        },
+      },
+    }),
+  ],
+  resolve: { alias: { src: resolve('src/') } },
+  test: {
+    environment: 'jsdom',
+    setupFiles: ['src/setupTests.ts'],
+    coverage: {
+      exclude: ['*.config.*', '*.d.ts'],
+    },
+  },
+});
